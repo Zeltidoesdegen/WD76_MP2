5c36eb27aa849690233291ba1367384373a60814
Zeltidoesdegen <ivan.carsula@gmail.com>
2023-08-31 12:46:31 +0800

    Edit in href links

--- cart.js
+++ cart.js
@@ -182,36 +182,3 @@ function updateTotalPrice() {
 //     // Update the total price
 //     cartTotal.textContent = total.toFixed(2);
 // }
-
-function displayCart(){
-  let totalPrice = 0;
-  let getCart = JSON.parse(localStorage.getItem("myCart"));
-  let showItems = "";
-  let items = JSON.parse(localStorage.getItem("myCart"));
-  document.getElementById('itemcount').innerHTML = items.length;
-  getCart.forEach(
-      function(items, index){
-          let price = items.pPrice.toLocaleString('en-PH', {
-              style: 'currency',
-              currency: 'PHP'
-          });
-          showItems = 
-          <div class="cartitem" id="cartitem">
-          <img src=${items.pImg} id="itemimage">
-          <h3 id="itemname">${items.pName}</h3>
-          <p id="itemprice"> ${price}</p>
-          <hr>
-          </div> + showItems ;
-
-          totalPrice += items.pPrice;
-      }
-  );
-  let finalPrice = Number(totalPrice).toLocaleString('en-PH', {
-      style: 'currency',
-      currency: 'PHP'
-      });
-  document.getElementById('cartlist').innerHTML = showItems;
-  document.getElementById('total').innerHTML = finalPrice;
-  document.getElementById('subtotal').innerHTML = finalPrice;
-}
-displayCart();
